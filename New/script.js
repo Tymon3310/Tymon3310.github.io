d6345fb7119f634569b3f4930aee030b621401c9
Tymon3310 <82083570+Tymon3310@users.noreply.github.com>
2023-05-31 12:46:32 +0200

    internal rework

--- New/script.js
+++ New/script.js
@@ -1,28 +1,28 @@
-window.onload = (event) => {
-  const resizeOps = () => {
-document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
-};
+// window.onload = (event) => {
+//   const resizeOps = () => {
+// document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+// };
 
-resizeOps();
-window.addEventListener("resize", resizeOps);
-};
+// resizeOps();
+// window.addEventListener("resize", resizeOps);
+// };
 
-var siteWidth = 1280;
-var scale = screen.width /siteWidth;
+// var siteWidth = 1280;
+// var scale = screen.width /siteWidth;
 
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+// document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
-const mobile = document.querySelector(".mobile"),
-pc = document.querySelector(".computer"),
-media = window.matchMedia("(max-width: 1000px)")
+// const mobile = document.querySelector(".mobile"),
+// pc = document.querySelector(".computer"),
+// media = window.matchMedia("(max-width: 1000px)")
 
-  if (media.matches) {
-mobile.style.display = "none"
-pc.style.display = "block"
- } else {
-pc.style.display = "none"
-mobile.style.display = "block" 
-}
+//   if (media.matches) {
+// mobile.style.display = "none"
+// pc.style.display = "block"
+//  } else {
+// pc.style.display = "none"
+// mobile.style.display = "block" 
+// }
 
 //theme switch
 
