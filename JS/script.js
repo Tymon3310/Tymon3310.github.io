e5b23f9509e2cc63bef4a74db09c04521990694f
Tymon3310 <82083570+Tymon3310@users.noreply.github.com>
2024-02-20 21:13:16 +0100

    Update code and add new icons

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    document.getElementById('aa').innerHTML = "hello js";
+};
+
 function abc() {
     document.getElementById('body').style.fontSize = '10000px'
     setTimeout(def, 3000)
@@ -6,4 +10,28 @@ function abc() {
 function def() {
     document.getElementById('body').style.fontSize = '14px'
     alert("Naprawiono")
+}
+
+function toggleMode() {
+    var content = document.getElementById('content');
+    var btn = document.getElementById('modebtn');
+    if (content.style.backgroundColor === 'white') {
+        content.style.backgroundColor = 'black';
+        btn.innerHTML = 'white mode';
+    } else {
+        content.style.backgroundColor = 'white';
+        btn.innerHTML = 'dark mode';
+    }
+}
+
+function toggleImage() {
+    var img = document.getElementById('win');
+    var btn = document.getElementById('winbtn');
+    if (img.src.match('win10.png')) {
+        img.src = 'win11.png';
+        btn.innerHTML = 'Downgrade';
+    } else {
+        img.src = 'win10.png';
+        btn.innerHTML = 'Upgrade';
+    }
 }
