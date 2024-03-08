414a40847750c73a0014e93230d740430a656db9
Tymon3310 <82083570+Tymon3310@users.noreply.github.com>
2024-03-08 23:25:52 +0100

    Add new files and update existing files

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+"use strict";
+window.onload = function () {
+    displayTime();
+};
 //langugage
 function en() {
     window.location.href = '/EN';
@@ -5,10 +9,22 @@ function en() {
 function pl() {
     window.location.href = '/';
 }
-//theme switch\
+//theme switch
 function light() {
     var elements = document.getElementsByClassName('content');
     for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle("dark-mode");
+        elements[i].classList.toggle("light-mode");
     }
 }
+function displayTime() {
+    console.log("time updated");
+    var options = { timeZone: "Europe/Warsaw", hour: "2-digit", minute: "2-digit", hour12: false };
+    var event = new Date();
+    var timeString = event.toLocaleTimeString('pl-PL', options);
+    var timeElement = document.getElementById('time');
+    if (timeElement !== null) {
+        timeElement.innerText = timeString;
+    }
+}
+// Run the function every minute
+setInterval(displayTime, 30000);
